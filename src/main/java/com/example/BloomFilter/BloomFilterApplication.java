980c98f10628e9dd7776c0b5e571dd071504824e
Vasanth Rajendran <varajendran@expedia.com>
2019-05-17 18:52:22 -0500

    Updating spring servlet initialization

--- src/main/java/com/example/BloomFilter/BloomFilterApplication.java
+++ src/main/java/com/example/BloomFilter/BloomFilterApplication.java
@@ -3,12 +3,19 @@ package com.example.BloomFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages="com.example.BloomFilter")
-public class BloomFilterApplication {
+public class BloomFilterApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BloomFilterApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloomFilterApplication.class, args);
